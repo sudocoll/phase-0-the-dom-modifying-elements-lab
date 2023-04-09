ac69c6ef7e10007d26430f0a33149b864b4b52cc
Collette Charles <collettecra123@gmail.com>
2023-04-09 07:22:45 -0400

    Complete Lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('H1');
+newHeader.id = "victory";
+
+const YOUR_NAME = "Collette Charles";
+newHeader.innerHTML = `${YOUR_NAME} is the champion`
